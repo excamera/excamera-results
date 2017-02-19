9c820b69a179647cf8b00a09579ada434060f22a
Sadjad Fouladi <sfouladi@gmail.com>
2017-02-19 19:31:06 +0000

    Update the run_all script.

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -6,8 +6,8 @@ import os
 import sys
 import time
 
-if len(sys.argv) != 6:
-    print("usage: run_all.py <kfdist> <chunksize> <start> <end> <region>")
+if len(sys.argv) != 7:
+    print("usage: run_all.py <kfdist> <chunksize> <quality_start> <quality_end> <quality_step> <region>")
     sys.exit(1)
 
 def runcommand(command):
@@ -15,24 +15,32 @@ def runcommand(command):
     return os.system(command)
 
 TOTAL_CHUNKS = 3552
+RUN_K = "REGION={region} FRAMES={num_frames} ~/excamera-results/scripts/run_K.sh {kfdist} {nworkers} {offset} {quality}"
 
-RUN_K = "REGION={region} ~/excamera-results/scripts/run_K.sh {kfdist} {nworkers} {offset} {quality}"
-
+kfdist = int(sys.argv[1])
+num_frames = int(sys.argv[2])
 start = int(sys.argv[3])
 end = int(sys.argv[4])
+step = int(sys.argv[5])
 
-NUM_WORKERS = [min(888, TOTAL_CHUNKS - i * 888) for i in range(4)]
+if num_frames == 12:
+    TOTAL_CHUNKS /= 2
+elif num_frames == 24:
+    TOTAL_CHUNKS /= 4
+
+NUM_WORKERS = [min(888, TOTAL_CHUNKS - i * 888) for i in range(TOTAL_CHUNKS / 888)]
 
 with open("run_log_%s" % time.time(), "w") as runlog:
-    for quality_level in range(start, end, 2):
-        for i in range(4):
+    for quality_level in range(start, end + 1, step):
+        for i in range(len(NUM_WORKERS)):
             try_count = 0
 
             while True:
                 time.sleep(10)
                 try_count += 1
-                retval = runcommand(RUN_K.format(region=sys.argv[5], 
-                                                 kfdist=sys.argv[1],
+                retval = runcommand(RUN_K.format(region=sys.argv[6],
+                                                 kfdist=kfdist,
+                                                 num_frames=num_frames,
                                                  nworkers=NUM_WORKERS[i],
                                                  offset=sum(NUM_WORKERS[:i]),
                                                  quality=quality_level))
@@ -40,10 +48,12 @@ with open("run_log_%s" % time.time(), "w") as runlog:
                 if retval != 0:
                     if try_count >= 5:
                         runlog.write("Failed: {} -> {}\n".format(quality_level, i))
+                        runlog.flush()
                         break
 
                 else:
                     runlog.write("Succeed: {} -> {}\n".format(quality_level, i))
+                    runlog.flush()
                     break
 
         runlog.write("\n")
