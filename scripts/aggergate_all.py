2f916c4f61918c23eb9b103348a14bd1671e1553
Sadjad Fouladi <sfouladi@gmail.com>
2017-02-24 00:07:17 -0800

    Fix the scripts.

---
+++ scripts/aggergate_all.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+
+from __future__ import division, print_function
+
+import os
+import sys
+
+from speed_run import y_values
+
+# s     => chunk size
+# k     => keyframe distance
+# movie => sintel | tears
+
+if len(sys.argv) != 4:
+    print("usage: {} <result-folder> <timing-folder> <output-folder>".format(sys.argv[0]))
+    sys.exit(1)
+
+RESULT_FOLDER = sys.argv[1]
+TIMING_FOLDER = sys.argv[2]
+OUTPUT_FOLDER = sys.argv[3]
+
+TARGET_MEASURE = "median"
+
+def generate_data_point(movie, s, k, target_measure="median"):
+    print("Processing {}-s{:02d}_k{:02d}".format(movie, s, k))
+
+    k_file_num = k
+
+    if k == 1:
+        if movie == "sintel":
+            k_file_num = 8
+        else:
+            k_file_num = 2
+
+    result_file = "{movie}-s{s:02d}_k{k:02d}.dat".format(movie=movie, s=s, k=k_file_num)
+    result_file = os.path.join(RESULT_FOLDER, result_file)
+    target_y = filter(lambda x: x[0] == k, y_values[movie][s])[0][1]
+
+    with open(result_file) as fin:
+        result_data = [x.strip() for x in fin.readlines()]
+
+    data = []
+    for i in range(len(result_data)):
+        if result_data[i].strip() == "# y{:02d}".format(target_y):
+            if k == 1:
+                data += result_data[i+2].split(" ")[0:2]
+            else:
+                data += result_data[i+2].split(" ")[2:4]
+
+    if not data:
+        raise Exception("Could not find SSIM vs Bitrate data for this.")
+
+    timing_file = "{movie}-s{s:02d}_k{k:02d}-total.stats".format(movie=movie, s=s, k=k)
+    timing_file = os.path.join(TIMING_FOLDER, timing_file)
+
+    with open(timing_file) as fin:
+        timing_data = [x.strip() for x in fin.readlines()]
+
+    if len(timing_data) < 2 or not timing_data[0].startswith("# "):
+        raise Exception("Invalid timing data.")
+
+    timing_data = dict(zip(timing_data[0][2:].strip().split(" "), timing_data[1].strip().split(" ")))
+    data += [timing_data[target_measure].strip()]
+
+    return data
+
+for movie in ["sintel", "tears"]:
+    for s in [6, 12]:
+        for k in [1, 2, 4, 8, 16]:
+            d = generate_data_point(movie=movie, s=s, k=k, target_measure=TARGET_MEASURE)
+
+            output_file = "{movie}-s{s:02d}_k{k:02d}.point".format(movie=movie, s=s, k=k)
+            output_file = os.path.join(OUTPUT_FOLDER, output_file)
+
+            with open(output_file, "w") as fout:
+                #fout.write("# bitrate (Mbits/s), mean SSIM (dB), {} time (s)\n".format(TARGET_MEASURE))
+                fout.write("{}\n".format("\t".join(d)))
--- scripts/speed_run.py
+++ scripts/speed_run.py
@@ -6,76 +6,95 @@ import os
 import sys
 import time
 
-if len(sys.argv) != 6:
-    print("usage: {} <movie=sintel|tears> <region> <chunk-size> <region-index> <repeat-count>".format(sys.argv[0]))
-    sys.exit(1)
-
 def runcommand(command):
     print(command)
     time.sleep(10)
     return os.system(command)
 
 y_values = {
-    6: (
-        (1, 32),
-        (2, 22),
-        (4, 16),
-        (8, 12),
-        (16, 12),
-    ),
-    12: (
-        (1, 26),
-        (2, 16),
-        (4, 12),
-        (8, 11),
-        (16, 11),
-    )
+    "sintel": {
+        6: (
+            (1, 32),
+            (2, 22),
+            (4, 16),
+            (8, 12),
+            (16, 12),
+        ),
+        12: (
+            (1, 26),
+            (2, 16),
+            (4, 12),
+            (8, 12),
+            (16, 10),
+        )
+    },
+    "tears": {
+        6: (
+            (1, 30),
+            (2, 24),
+            (4, 20),
+            (8, 18),
+            (16, 16),
+        ),
+        12: (
+            (1, 28),
+            (2, 20),
+            (4, 18),
+            (8, 16),
+            (16, 16),
+        )
+    },
 }
-
-movie = sys.argv[1]
-region = sys.argv[2]
-num_frames = int(sys.argv[3])
-idx = int(sys.argv[4])
-REPEAT_TIMES = int(sys.argv[5])
-
-assert(movie == "sintel" or movie == "tears")
-assert((num_frames == 6 and idx <= 3) or (num_frames == 12 and idx <= 1))
-
-if movie == "sintel":
-    TOTAL_CHUNKS = 3552
-
-    if num_frames == 6:
-        NUM_WORKERS = [880, 896, 880, 896]
-    elif num_frames == 12:
-        NUM_WORKERS = [880, 896]
-    elif num_frames == 24:
-        NUM_WORKERS = [888]
-else:
-    TOTAL_CHUNKS = 2936
-
-    if num_frames == 6:
-        NUM_WORKERS = [736, 736, 736, 728]
-    elif num_frames == 12:
-        NUM_WORKERS = [736, 732]
-    elif num_frames == 24:
-        NUM_WORKERS = [734]
 
 RUN_K = "NOUPLOAD=1 REGION={region} FRAMES={num_frames} ~/excamera-results/scripts/run_K.sh {movie} {kfdist} {nworkers} {offset} {quality}"
 
-with open("run_log_%s" % time.time(), "w") as runlog:
-    for K, y in y_values[num_frames]:
-        for i in range(REPEAT_TIMES):
-            print("Speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K))
+if __name__ == '__main__':
+    if len(sys.argv) != 6:
+        print("usage: {} <movie=sintel|tears> <region> <chunk-size> <region-index> <repeat-count>".format(sys.argv[0]))
+        sys.exit(1)
 
-            retval = runcommand(RUN_K.format(region=region,
-                                             kfdist=K,
-                                             num_frames=num_frames,
-                                             nworkers=NUM_WORKERS[idx],
-                                             offset=sum(NUM_WORKERS[:idx]),
-                                             quality=y,
-                                             movie=movie))
+    movie = sys.argv[1]
+    region = sys.argv[2]
+    num_frames = int(sys.argv[3])
+    idx = int(sys.argv[4])
+    REPEAT_TIMES = int(sys.argv[5])
 
-            if retval == 0:
-                print("[OK] speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K), file=runlog)
-            else:
-                print("[FAIL] speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K), file=runlog)
+    assert(movie == "sintel" or movie == "tears")
+    assert((num_frames == 6 and idx <= 3) or (num_frames == 12 and idx <= 1))
+
+    if movie == "sintel":
+        TOTAL_CHUNKS = 3552
+
+        if num_frames == 6:
+            NUM_WORKERS = [880, 896, 880, 896]
+        elif num_frames == 12:
+            NUM_WORKERS = [880, 896]
+        elif num_frames == 24:
+            NUM_WORKERS = [888]
+    else:
+        TOTAL_CHUNKS = 2936
+
+        if num_frames == 6:
+            NUM_WORKERS = [736, 736, 736, 728]
+        elif num_frames == 12:
+            NUM_WORKERS = [736, 732]
+        elif num_frames == 24:
+            NUM_WORKERS = [734]
+
+    with open("run_log_%s" % time.time(), "w") as runlog:
+        for K, y in y_values[movie][num_frames]:
+            for i in range(REPEAT_TIMES):
+                print("Speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K))
+
+                retval = runcommand(RUN_K.format(region=region,
+                                                 kfdist=K,
+                                                 num_frames=num_frames,
+                                                 nworkers=NUM_WORKERS[idx],
+                                                 offset=sum(NUM_WORKERS[:idx]),
+                                                 quality=y,
+                                                 movie=movie))
+
+                if retval == 0:
+                    print("[OK] speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K), file=runlog)
+                else:
+                    print("[FAIL] speed test ({}) for s{:02d}_k{:02d}".format(i, num_frames, K), file=runlog)
