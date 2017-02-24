071c8fed28f7ec98b173d710f295bb31f05ad973
Sadjad Fouladi <sfouladi@gmail.com>
2017-02-23 18:28:23 -0800

    Add scripts for generating timing information.

---
+++ scripts/do_all_timing.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <root-folder> <result-folder>"
+    exit 1
+fi
+
+sintel[06-0]="xcenc_transitions_k*_n880_o0_*"
+sintel[06-1]="xcenc_transitions_k*_n896_o880_*"
+sintel[06-2]="xcenc_transitions_k*_n880_o1776_*"
+sintel[06-3]="xcenc_transitions_k*_n896_o2656_*"
+
+sintel[12-0]="xcenc_transitions_k*_n880_o0_*"
+sintel[12-1]="xcenc_transitions_k*_n896_o880_*"
+
+tears[06-0]="xcenc_transitions_k*_n736_o0_*"
+tears[06-1]="xcenc_transitions_k*_n736_o736_*"
+tears[06-2]="xcenc_transitions_k*_n736_o1472_*"
+tears[06-3]="xcenc_transitions_k*_n728_o2208_*"
+
+tears[12-0]="xcenc_transitions_k*_n736_o0_*"
+tears[12-1]="xcenc_transitions_k*_n732_o736_*"
+
+ROOT_FOLDER=$1
+RES_FOLDER=$2
+
+mkdir -p ${RES_FOLDER}
+mkdir -p ${RES_FOLDER}/final
+
+for k in 01 02 04 08 16
+do
+  for s in 06 12
+  do
+    for movie in sintel tears
+    do
+      echo "Processing ${movie}-s${s}_k${k}..."
+
+      FOLDER=${ROOT_FOLDER}/${movie}-s${s}_k${k}-logs
+
+      if [ -d "${FOLDER}" ]
+      then
+        if [ "${s}" -eq "06" ]
+        then
+          for i in 0 1 2 3
+          do
+            LOGFILE=$movie[${s}-${i}]
+            ./xc-time.py region ${FOLDER}/${!LOGFILE} > ${RES_FOLDER}/${movie}-s${s}_k${k}-${i}.stats
+          done
+        elif [ "${s}" -eq "12" ]
+        then
+          for i in 0 1
+          do
+            LOGFILE=$movie[${s}-${i}]
+            ./xc-time region ${FOLDER}/${!LOGFILE} > ${RES_FOLDER}/${movie}-s${s}_k${k}-${i}.stats
+          done
+        fi
+
+        ./xc-time.py total ${RES_FOLDER}/${movie}-s${s}_k${k}-*.stats >${RES_FOLDER}/final/${movie}-s${s}_k${k}-total.stats
+      fi
+    done
+  done
+done
---
+++ scripts/xc-time.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import statistics
+import pdb
+
+def read_log_file(path):
+    stats = {'total_time': 0}
+
+    with open(path) as fin:
+        for line in fin:
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            line = line.split(":", 1)[1]
+            data = eval(line)
+
+            assert(data[-1][1] == "FinalState")
+
+            final_time = data[-1][0]
+
+            stats['total_time'] = max(stats['total_time'], final_time)
+
+    return stats
+
+def process_repetitive_runs(log_files):
+    all_runs = []
+
+    for log_file in log_files:
+        all_runs += [read_log_file(log_file)['total_time']]
+
+    return {
+        'min': min(all_runs),
+        'max': max(all_runs),
+        'avg': statistics.mean(all_runs),
+        'median': statistics.median(all_runs),
+        #'stdev': statistics.stdev(all_runs)
+    }
+
+def process_whole_run(stat_files):
+    all_stats = []
+
+    for stat_file in stat_files:
+        with open(stat_file) as fin:
+            all_stats += [json.load(fin)['median']]
+
+    return (
+        ('min', min(all_stats)),
+        ('max', max(all_stats)),
+        ('avg', statistics.mean(all_stats)),
+        ('median', statistics.median(all_stats)),
+        #('stdev', statistics.stdev(all_stats)),
+    )
+
+
+command = sys.argv[1]
+
+if command == "region":
+    print(json.dumps(process_repetitive_runs(sys.argv[2:])))
+elif command == "total":
+    data = process_whole_run(sys.argv[2:])
+
+    print(" ".join([str(x[1]) for x in data]))
